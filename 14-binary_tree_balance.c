bfb28b6e9d24af2c2cfcfe381917e74477f9b3b9
Bayero-abdul <bayeroa65@gmail.com>
2022-11-15 20:50:24 +0000

    Return 0 if tree is NULL

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,6 +11,9 @@ int binary_tree_balance(const binary_tree_t *tree)
 {
 	int l, r;
 
+	if (tree == NULL)
+		return (0);
+
 	l = binary_tree_height(tree->left);
 	r = binary_tree_height(tree->right);
 	return (l - r);
