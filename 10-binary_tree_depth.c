a144797a729577cac14cc08234c679388839d045
Bayero-abdul <bayeroa65@gmail.com>
2022-11-15 16:08:38 +0000

    Return 0 if tree is NULL

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,8 +8,12 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	if (tree != NULL)
-		return (binary_tree_depth(tree->parent) + 1);
+	if (tree == NULL)
+		return (0);
 
-	return (-1);
+	if (tree->parent == NULL)
+		return (0);
+
+	return (binary_tree_depth(tree->parent) + 1);
+
 }
